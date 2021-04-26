15be8615d1a153c9edb7e859d26b0aa7eb47feec
Brian <blsvsanteliz@gmail.com>
2021-04-25 21:28:02 -0400

    Delete about route, modify tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
-  darkMode: false, // or 'media' or 'class'
+  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
     extend: {},
+    container: {
+      center: true,
+      padding: "2rem"
+    }
   },
-  plugins: [],
+  plugins: []
 };
